05cebab6e2c2a7bbdbe88085e5ddebad1ea0e586
atharva@cool <katharva01@gmail.com>
2021-07-22 19:16:50 +0530

    knapsack done

--- day7/Knapsack.java
+++ day7/Knapsack.java
@@ -1,5 +1,27 @@
-package day7;
 
-public class Knapsack {
-    
+
+public class Knapsack{
+    public static double fillSack(double[][] arr,double capacity){
+        SortByRatio.sortByRatio(arr);
+        double profit=0;
+        for(int i=0;i<arr[1].length;i++){
+            if(arr[1][i]<=capacity){
+                profit=profit+arr[0][i];
+                capacity=capacity-arr[1][i];
+            }
+            else if(arr[1][i]>capacity){
+                profit=(capacity/arr[1][i])*arr[0][i]+profit;
+                capacity=0;
+            }
+            
+            if(capacity==0){break;}
+        }
+        
+        return profit;
+    }
+
+    public static void main(String[] args) {
+        double[][] arr = new double[][] { {24,44,20,21,12,35,5,36}, {8,11,8,7,2,7,1,6 } };
+        System.out.printf("Maximum profit after filling given items %naccording to capacity of knapsack is : %f",fillSack(arr,30));
+    }
 }
--- day7/SortByRatio.java
+++ day7/SortByRatio.java
@@ -1,14 +1,12 @@
-// this sorting technique is useful in sorting jobs
-// according to profit in job scheduling problem
 
-public class TwoDSort {
-    public static void sort(double[][] arr) {
+public class SortByRatio {
+    public static void sortByRatio(double[][] arr) {
         int k = 0;
 
         for (int j = k; j < arr[0].length; j++) {
             int temp = j;
             for (int i = k; i < arr[0].length; i++) {
-                if (arr[0][i] > arr[0][temp]) {
+                if (arr[0][i]/arr[1][i] > arr[0][temp]/arr[1][temp]) {
                     temp = i;
                 }
                 
@@ -34,14 +32,17 @@ public class TwoDSort {
     }
 
     public static void main(String[] args) {
-        double[][] arr = new double[][] { { 20, 15, 10, 5,8,1,9, 22 }, { 2, 2, 1,3,4, 3, 3, 4 } };
+        double[][] arr = new double[][] { {24,44,69,21,12,35,5,36}, {8,11,23,7,2,7,1,6 } };
+        System.out.println("Array before sorting : ");
         for (int j = 0; j < arr.length; j++) {
             for (int i = 0; i < arr[0].length; i++) {
                 System.out.print(arr[j][i] + " ");
             }
             System.out.println();
         }
-        sort(arr);
+        sortByRatio(arr);
+        System.out.println("Array After sorting : ");
         printTwoDArray(arr);
     }
 }
+
